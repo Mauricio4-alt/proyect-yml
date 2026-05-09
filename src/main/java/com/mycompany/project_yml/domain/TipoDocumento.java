13869f3088fc621bdcd181b799f43f68bddcd1b2
Hector_M <escaa2714@gmail.com>
2026-05-08 21:25:40 -0500

    chore:ultimos detalles

--- src/main/java/com/mycompany/project_yml/domain/TipoDocumento.java
+++ src/main/java/com/mycompany/project_yml/domain/TipoDocumento.java
@@ -1,10 +1,25 @@
 package com.mycompany.project_yml.domain;
 
+import jakarta.annotation.Nonnull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "tipo_documentos")
 public class TipoDocumento {
+
+    @Id
     private String id;
-    private String  siglas;
+
+    @Nonnull
+    @Field("asdaf")
+    private String siglas;
+
+    @Field("nombre_documento")
     private String nombreDocumento;
+
     private String estado;
+
     public TipoDocumento(String siglas,String nombreDocumento,String estado){
         this.siglas= siglas;
         this.nombreDocumento = nombreDocumento;
